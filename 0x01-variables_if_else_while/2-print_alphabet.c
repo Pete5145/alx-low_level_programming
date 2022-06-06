3bb1728135a4b79a953db3d034125cb8a32dad2a
pete5145 <peterosakpolor661@gmail.com>
2022-06-06 11:41:10 -0700

    2-print_alphabet.c file added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 int alphabet;
+	 scanf("%c", alphabet);
+	 putschar(tolower(alphabet);
+	 
+	 return (0);
+}
