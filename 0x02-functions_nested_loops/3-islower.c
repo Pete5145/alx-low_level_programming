2560a4648305ea621ab2aeaa7b4aabe676dd4218
pete5145 <peterosakpolor661@gmail.com>
2022-06-07 18:29:35 -0700

    3-islower.c file added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: checks if a character is lowercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _islower(int c)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+
+		if (alphabet == 'c')
+		{
+			return (1);
+		}
+		else
+		{
+			return(0);
+		}
+	}
+	return (0);
+}
