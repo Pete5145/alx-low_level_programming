75db7e37f1534c27b4d2ff0cc69c46e73a9f841a
pete5145 <peterosakpolor661@gmail.com>
2022-06-09 12:27:50 -0700

    3-print_numbers.c updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0123456789\n
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+}
