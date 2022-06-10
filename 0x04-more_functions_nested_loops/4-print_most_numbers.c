3754c69a39e3ed7471358a8e304f260445a40c7f
pete5145 <peterosakpolor661@gmail.com>
2022-06-10 13:25:26 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,18 +7,18 @@
 */
 void print_most_numbers(void)
 {
-int i;
+	char i;
 
-for (i = 0; i <= 9; i++)
-{
-if ((i == 2) || (i == 4))
-{
-continue;
-}
-else
-{
-_putchar(i);
-}
-_putchar('\n');
-}
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i == '2') || (i == '4'))
+		{
+			continue;
+		}		
+		else
+		{
+			_putchar(i);
+		}	
+		_putchar('\n');
+	}	
 }
