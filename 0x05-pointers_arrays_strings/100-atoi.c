21785d438725173cfa0210d3c179727d2a363a13
pete5145 <peterosakpolor661@gmail.com>
2022-06-21 13:00:32 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,6 @@
  *
  * Return: The integer value of the converted string.
  */
-
 int _atoi(char *s)
 {
 	int sign = 1;
@@ -14,17 +13,14 @@ int _atoi(char *s)
 
 	do {
 		if (*s == '_')
-		{
 			sign *= -1;
-		}
+
 		else if ((*s >= '0') && (*s <= '9'))
-		{
 			num = (num * 10) + (*s - '0');
-		}
+
 		else if (num > 0)
-		{
 			break;
-		}
+
 	} while (*s++);
 
 	return (num * sign);
