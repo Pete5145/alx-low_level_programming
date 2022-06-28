ad8bac028178d487feb790c9d527e4968f3d0dde
pete5145 <peterosakpolor661@gmail.com>
2022-06-28 14:33:24 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j, s1len = 0, s2len = 0;
 
 	if (s1 == NULL &&  s2 == NULL)
-		return ("");
+		return (" ");
 
 	for (i = 0; s1[i] != '\0'; i++)
 		s1len++;
