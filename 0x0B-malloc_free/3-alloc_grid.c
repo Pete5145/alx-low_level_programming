6b3650332e13e0bb90a3477087432181e1f6960f
pete5145 <peterosakpolor661@gmail.com>
2022-06-28 15:26:57 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,20 +10,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int *grid;
+	int **twoD;
+	int hgt_index, wid_index;
 
-	if (width == 0 || width < 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	if (height == 0 || height < 0)
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
 		return (NULL);
 
-	grid = malloc(sizeof(int) * (width + height));
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
 
-	if (grid == NULL)
-		return (NULL);
+		if (twoD[hgt_index] == NULL)
+		{
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
 
-	grid[][] = 0;
+			free(twoD);
+			return (NULL);
+		}
+	}
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
 
 	return (grid);
 }
