d715a04601bab1e4877bb8927d143619fac6b399
pete5145 <peterosakpolor661@gmail.com>
2022-06-29 01:44:11 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,8 +16,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	product = argv[1] * argv[2];
-	printf("%d\n", product);
+	else
+	{
+		product = argv[1] * argv[2];
+		printf("%d\n", product);
+	}
 
 	return (0);
 }
