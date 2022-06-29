1b7732e4272073d280737b75e4dd3baa16e9aba6
pete5145 <peterosakpolor661@gmail.com>
2022-06-29 01:26:13 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of the program.
+ * @argc: Number of command line arguments passed to the program.
+ * @argv: Array of arguments.
+ *
+ * Return: Always(0).
+ */
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	product = argv[1] * argv[2];
+	printf("%d\n", product);
+
+	return (0);
+}
