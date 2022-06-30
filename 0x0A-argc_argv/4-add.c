6289543d2c57daa801dbf2d6c25d6f4d1679dafa
pete5145 <peterosakpolor661@gmail.com>
2022-06-30 10:57:46 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,17 +12,15 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		if (!(argv[i] >= '0') || !(argv[i] <= '9'))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		if (argv[i] > 0)
-		{
+
 			sum += atoi(argv[i]);
-		}
 	}
 
 	printf("%d\n", sum);
